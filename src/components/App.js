16865e04d7446f6d325418e943ffc7e701672676
piiyussh <116825106+piiyussh@users.noreply.github.com>
2026-04-01 09:36:16 +0530

    Add Review component to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,22 @@
 <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+import React from "react";
+import Review from "./Review";
+
+function App() {
+  return (
+    <main>
+      <section className="container">
+        
+        
+        <h2 id="review-heading">Our Reviews</h2>
+
+        
+        <Review />
+
+      </section>
+    </main>
+  );
+}
+
+export default App;
